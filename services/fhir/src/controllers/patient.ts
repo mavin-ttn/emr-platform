2e2eebeca3b8d0a17ce6b82b3444ae31d3a074c6
Agam Agarwal <agam.agarwal@tothenew.com>
2025-04-22 18:14:10 +0530

    fixed create patient api

--- services/fhir/src/controllers/patient.ts
+++ services/fhir/src/controllers/patient.ts
@@ -1,10 +1,17 @@
 import { Request, Response } from 'express';
-import { getPatientDetails, getMedicationRequests, createPatient } from '../services/patient';
+import {
+  getPatientDetails,
+  getMedicationRequests,
+  createPatient,
+} from '../services/patient';
 
-export const fetchPatientDetails = async (req: Request, res: Response): Promise<void> => {
+export const fetchPatientDetails = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
   const { userId } = req.params;
   try {
-    const authHeader = req.headers?.authorization || ''
+    const authHeader = req.headers?.authorization || '';
     const patientDetails = await getPatientDetails(userId, authHeader);
     res.status(200).json(patientDetails);
   } catch (error: any) {
@@ -12,21 +19,31 @@ export const fetchPatientDetails = async (req: Request, res: Response): Promise<
   }
 };
 
-export const fetchMedicationRequests = async (req: Request, res: Response): Promise<void> => {
+export const fetchMedicationRequests = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
   const { patientId } = req.params;
   try {
-    const authHeader = req.headers?.authorization || ''
-    const medicationRequests = await getMedicationRequests(patientId, authHeader);
+    const authHeader = req.headers?.authorization || '';
+    const medicationRequests = await getMedicationRequests(
+      patientId,
+      authHeader
+    );
     res.status(200).json(medicationRequests);
   } catch (error: any) {
     res.status(500).json({ message: error.message });
   }
 };
 
-export const createPatientRequest = async (req: Request, res: Response): Promise<void> => {
+export const createPatientRequest = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
   const patientData = req.body;
+  console.log(patientData, 'patientDatat');
   try {
-    const authHeader = req.headers?.authorization || ''
+    const authHeader = req.headers?.authorization || '';
     const patientDetails = await createPatient(patientData, authHeader);
     res.status(201).json(patientDetails);
   } catch (error: any) {
--- services/fhir/src/routes/routes.ts
+++ services/fhir/src/routes/routes.ts
@@ -1,14 +1,15 @@
 import express from 'express';
 import {
-    fetchPatientDetails, fetchMedicationRequests, createPatientRequest
-} from '../controllers/patient'
-import {fetchProviderDetails } from '../controllers/provider'
+  fetchPatientDetails,
+  fetchMedicationRequests,
+  createPatientRequest,
+} from '../controllers/patient';
+import { fetchProviderDetails } from '../controllers/provider';
 
 const router = express.Router();
 
-
 router.get('/Patient/:userId', fetchPatientDetails);
 router.get('/medication-request/:patientId', fetchMedicationRequests);
-router.post('/patient/create', createPatientRequest)
+router.post('/patient/create', createPatientRequest);
 router.get('/Practitioner/:userId', fetchProviderDetails);
 export default router;
