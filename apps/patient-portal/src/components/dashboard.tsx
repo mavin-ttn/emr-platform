0ff4686101173a0a33dabe5fe7ed5bbb0c879148
Shivam Gupta <shivam.gupta@tothenew.com>
2025-04-22 15:40:25 +0530

    added patient service

--- apps/patient-portal/src/components/dashboard.tsx
+++ apps/patient-portal/src/components/dashboard.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
-import { useState, useEffect } from 'react';
-import axios from 'axios';
-import CreatePatient from './CreatePatient';
-import ROLE from '../constants/index';
+import React from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import CreatePatient from "./CreatePatient";
+import ROLE from "../constants/index";
+import Button from "./Button";
 
 interface PatientInfo {
   name: string;
@@ -15,29 +16,37 @@ interface PatientInfo {
   language: string;
   practitioner: string;
   organization: string;
+  id: string | null | undefined;
+}
+
+interface MedicationRequest {
+  therapyType: string;
+  dosageInstruction: string;
 }
 
 function Dashboard() {
   const [patient, setPatient] = useState<PatientInfo | null>(null);
-  const userRole = localStorage.getItem('userRole');
+  const [medicationRequest, setMedicationRequest] =
+    useState<MedicationRequest | null>(null);
+  const userRole = localStorage.getItem("userRole");
 
   useEffect(() => {
     const fetchPatient = async () => {
-      const token = localStorage.getItem('access_token');
+      const token = localStorage.getItem("access_token");
       const patientId =
-        localStorage.getItem('patient_id') === 'undefined'
+        localStorage.getItem("patient_id") === "undefined"
           ? undefined
-          : localStorage.getItem('patient_id');
-      const userId = patientId || localStorage.getItem('id');
+          : localStorage.getItem("patient_id");
+      const userId = patientId || localStorage.getItem("id");
 
       if (!token) {
-        console.warn('No access token found!');
+        console.warn("No access token found!");
         return;
       }
 
       try {
         const response = await axios.get(
-          `https://fhir.epic.com/interconnect-fhir-oauth/api/FHIR/R4/${userRole}/${userId}`,
+          `http://localhost:3000/v2/${userRole}/${userId}`,
           {
             headers: {
               Authorization: `Bearer ${token}`,
@@ -50,45 +59,83 @@ function Dashboard() {
         const nameEntry = data.name?.[0] || {};
         const addressEntry = data.address?.[0] || {};
         const telecomPhone = data.telecom?.find(
-          (t: any) => t.system === 'phone'
+          (t: any) => t.system === "phone"
         );
         const telecomEmail = data.telecom?.find(
-          (t: any) => t.system === 'email'
+          (t: any) => t.system === "email"
         );
 
         setPatient({
           name:
             nameEntry.text ||
-            `${nameEntry.given?.join(' ')} ${nameEntry.family}`,
+            `${nameEntry.given?.join(" ")} ${nameEntry.family}`,
           gender: data.gender,
           birthDate: data.birthDate,
-          address: addressEntry.line?.join(', ') + ', ' + addressEntry.city,
-          phone: telecomPhone?.value || '',
-          email: telecomEmail?.value || '',
-          maritalStatus: data.maritalStatus?.text || 'N/A',
-          language: data.communication?.[0]?.language?.text || 'N/A',
-          practitioner: data.generalPractitioner?.[0]?.display || 'N/A',
-          organization: data.managingOrganization?.display || 'N/A',
+          address: addressEntry.line?.join(", ") + ", " + addressEntry.city,
+          phone: telecomPhone?.value || "",
+          email: telecomEmail?.value || "",
+          maritalStatus: data.maritalStatus?.text || "N/A",
+          language: data.communication?.[0]?.language?.text || "N/A",
+          practitioner: data.generalPractitioner?.[0]?.display || "N/A",
+          organization: data.managingOrganization?.display || "N/A",
+          id: patientId,
         });
       } catch (err) {
-        console.error('Failed to fetch patient data:', err);
+        console.error("Failed to fetch patient data:", err);
       }
     };
 
     fetchPatient();
   }, []);
 
+  useEffect(() => {
+    console.log("medication request state- ", medicationRequest);
+  }, [medicationRequest]);
+
+  const getMedicationRequest = async () => {
+    const token = localStorage.getItem("access_token");
+    const patientId =
+      localStorage.getItem("patient_id") === "undefined"
+        ? undefined
+        : localStorage.getItem("patient_id");
+
+    if (!token) {
+      console.warn("No access token found!");
+      return;
+    }
+
+    try {
+      const response = await axios.get(
+        `https://fhir.epic.com/interconnect-fhir-oauth/api/FHIR/R4/MedicationRequest?patient=${patientId}`,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+
+      const data = response.data;
+      console.log("medication request - >", data);
+      setMedicationRequest({
+        therapyType: data.entry[0].resource.courseOfTherapyType.text,
+        dosageInstruction: data.entry[0].resource.dosageInstruction[0].text,
+      });
+    } catch (err) {
+      console.error("Failed to fetch patient data:", err);
+    }
+  };
+
   if (!patient)
     return (
       <div className="text-center mt-20">
-        Loading {userRole === ROLE.PATIENT ? 'Patient ' : 'Provider'} info...
+        Loading {userRole === ROLE.PATIENT ? "Patient " : "Provider"} info...
       </div>
     );
 
   return (
     <div className="dashboard">
       <h1 className="dashboard-title text-2xl font-bold my-6 text-center">
-        {userRole === ROLE.PATIENT ? 'Patient ' : 'Provider'} Dashboard
+        {userRole === ROLE.PATIENT ? "Patient " : "Provider"} Dashboard
       </h1>
 
       {userRole === ROLE.PRACTITIONER ? <CreatePatient /> : null}
@@ -97,11 +144,27 @@ function Dashboard() {
         {Object.entries(patient).map(([key, value]) => (
           <div key={key} className="card p-4 border rounded shadow">
             <p className="card-label font-semibold">
-              {key.replace(/([A-Z])/g, ' $1')}
+              {key.replace(/([A-Z])/g, " $1")}
             </p>
             <p className="card-value text-gray-700">{value}</p>
           </div>
         ))}
+      </div>
+
+      <Button
+        label="Get Medication Request"
+        onClick={() => getMedicationRequest()}
+      />
+      <div className="card-grid grid grid-cols-2 gap-4 p-4">
+        {medicationRequest &&
+          Object.entries(medicationRequest).map(([key, value]) => (
+            <div key={key} className="card p-4 border rounded shadow">
+              <p className="card-label font-semibold">
+                {key.replace(/([A-Z])/g, " $1")}
+              </p>
+              <p className="card-value text-gray-700">{value}</p>
+            </div>
+          ))}
       </div>
     </div>
   );
