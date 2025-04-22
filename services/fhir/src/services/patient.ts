211750def5753937ac2a8e98df8fadd14e19b7e0
Agam Agarwal <agam.agarwal@tothenew.com>
2025-04-22 18:14:10 +0530

    fixed create patient api

--- services/fhir/src/services/patient.ts
+++ services/fhir/src/services/patient.ts
@@ -1,6 +1,9 @@
 import axios from 'axios';
 
-export const getPatientDetails = async (userId: string, authHeader: string): Promise<any> => {
+export const getPatientDetails = async (
+  userId: string,
+  authHeader: string
+): Promise<any> => {
   const baseURL = `https://fhir.epic.com/interconnect-fhir-oauth/api/FHIR/R4/Patient/${userId}`;
   try {
     const response = await axios.get(baseURL, {
@@ -19,37 +22,46 @@ export const getPatientDetails = async (userId: string, authHeader: string): Pro
  * Fetch medication requests for a patient
  * @param patientId - The ID of the patient
  */
-export const getMedicationRequests = async (patientId: string, authHeader: string): Promise<any> => {
-    const baseURL = `https://fhir.epic.com/interconnect-fhir-oauth/api/FHIR/R4/MedicationRequest?patient=${patientId}`;
-    try {
-      const response = await axios.get(baseURL, {
-        headers: {
-          Authorization: `${authHeader}`,
-        },
-      });
-      return response.data;
-    } catch (error: any) {
-      console.error('Error fetching medication requests:', error.message);
-      throw new Error('Failed to fetch medication requests');
-    }
-  };
+export const getMedicationRequests = async (
+  patientId: string,
+  authHeader: string
+): Promise<any> => {
+  const baseURL = `https://fhir.epic.com/interconnect-fhir-oauth/api/FHIR/R4/MedicationRequest?patient=${patientId}`;
+  try {
+    const response = await axios.get(baseURL, {
+      headers: {
+        Authorization: `${authHeader}`,
+      },
+    });
+    return response.data;
+  } catch (error: any) {
+    console.error('Error fetching medication requests:', error.message);
+    throw new Error('Failed to fetch medication requests');
+  }
+};
 
-  export const createPatient = async (patientData: string, authHeader: string): Promise<any> => {
-    const baseURL = 'https://fhir.epic.com/interconnect-fhir-oauth/api/FHIR/R4/Patient';
-    console.log(patientData, authHeader)
-    try {
-      const response = await axios.post(baseURL, {
-          method: 'POST',
-          headers: {
-            Authorization: `${authHeader}`,
-            'Content-Type': 'application/fhir+json',
-            Accept: 'application/fhir+json',
-          },
-          body: JSON.stringify(patientData),
-      });
-      return response.data;
-    } catch (error: any) {
-      console.error('Error fetching patient details:', error);
-      throw new Error('Failed to fetch patient details');
-    }
-  };
+export const createPatient = async (
+  patientData: string,
+  authHeader: string
+): Promise<any> => {
+  const baseURL =
+    'https://fhir.epic.com/interconnect-fhir-oauth/api/FHIR/R4/Patient';
+  console.log(patientData, authHeader, baseURL, 'agambase');
+  try {
+    const response = await axios.post(baseURL, patientData, {
+      method: 'POST',
+      headers: {
+        Authorization: `${authHeader}`,
+        'Content-Type': 'application/fhir+json',
+        Accept: 'application/fhir+json',
+      },
+    });
+    return response.data;
+  } catch (error: any) {
+    console.error(
+      'Error fetching patient details:',
+      error.response?.data || error.message
+    );
+    throw new Error('Failed to fetch patient details');
+  }
+};
