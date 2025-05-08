5f88399a76777d779af375566837f8c05808be30
tanwar-harsh <harshtanwar25@gmail.com>
2025-05-08 17:10:24 +0530

    Refactor: updated patients data

--- apps/patient-portal/src/constants/index.ts
+++ apps/patient-portal/src/constants/index.ts
@@ -1,15 +1,11 @@
 export const ROLE = {
-  PATIENT: 'patient',
-  PRACTITIONER: 'practitioner',
-  PRACTITIONER_STU3: 'practitionerStu3',
+  PATIENT: "patient",
+  PRACTITIONER: "practitioner",
+  PRACTITIONER_STU3: "practitionerStu3",
 };
 
 export const PATIENTS_DATA = [
-  { id: "erXuFYUfucBZaryVksYEcMg3", name: "Patient 1" },
-  { id: "erXuFYUfucBZaryVksYEcMg3", name: "Patient 2" },
-  { id: "erXuFYUfucBZaryVksYEcMg3", name: "Patient 3" },
+  { id: "erXuFYUfucBZaryVksYEcMg3", name: "Camila" },
+  { id: "eq081-VQEgP8drUUqCWzHfw3", name: "Derrick" },
+  { id: "eAB3mDIBBcyUKviyzrxsnAw3", name: "Desiree" },
 ];
-
-
-
-
